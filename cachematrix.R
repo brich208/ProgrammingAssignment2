f7bdfab03d655ca81e5a9eb1a1146f640896ac5b
brich208 <rich_brian@bah.com>
2015-10-23 13:48:23 -0400

    Commit after completing Programming Assignment 2 for rpro-033

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions first create a matrix in the first function
+## then solve for the inverse of that matrix in the second function. The first
+## function allows the user to pull the original matrix (getMat), the inverse
+## matrix (getInv), to solve for the inverse (setInv), and pull the inverse
+## (getInv). The user can also change the value of an existing matrix (setMat).
 
-## Write a short comment describing this function
+## The following functions creates a matrix and also contains a list of 
+## functions to retrieve the matrix (getMat), retrive the inverse matrix 
+## (getInv), set the inverse matrix (setInv), and change an exisiting 
+## matrix (setMat).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  setMat <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  getMat <- function() x
+  setInv <- function(solve) m <<- solve
+  getInv <- function() m
+  list(setMat = setMat, getMat = getMat,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## The following function first checks to see if the inverse already exists,
+## and returns the inverse from the cache if it does. If the inverse does
+## not exist in the cache the function uses setInv to solve for the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  data <- x$getMat()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
